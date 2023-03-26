c09fdd6f4ede17cd352bda1161d0005be54d34c0
ESHAN GUPTA <es66han66@gmail.com>
2023-03-26 22:13:46 +0530

    MongoDB Express Node CRUD Application

---
+++ MongoDB_Express_Node_CRUD/Users/UserService.js
@@ -0,0 +1,37 @@
+export default class UserService {
+  constructor(central) {
+    this.central = central;
+    this.userDB = central.DBs["users"];
+  }
+
+  async createUser(userData) {
+    const response = await this.userDB.DBQuery("user", "insertOne", userData);
+    return response;
+  }
+
+  async updateUser(userId, userData) {
+    const response = await this.userDB.DBQuery(
+      "user",
+      "updateOne",
+      {
+        id: userId,
+      },
+      userData
+    );
+    return response;
+  }
+
+  async getUser(userId) {
+    const response = await this.userDB.DBQuery("user", "findOne", {
+      id: userId,
+    });
+    return response;
+  }
+
+  async deleteUser(userId) {
+    const response = await this.userDB.DBQuery("user", "delete", {
+      id: userId,
+    });
+    return response;
+  }
+}
