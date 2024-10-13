b7c499ffaf82cf42ca609ecc17ee313fb3748088
ESHAN GUPTA <es66han66@gmail.com>
2024-10-13 20:10:58 +0530

    custom logger

---
+++ custom-logger/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const net = require('net')
+
+const logger = require('./logger')('server')
+
+const server = net.createServer()
+const port = 8000
+const host = '127.0.0.1'
+
+server.on('connection', socket => {
+  logger.log('CLient connected')
+  socket.on('data', data => {
+    const reqData = data.toString()
+    logger.log(reqData)
+
+    socket.write('res: ', reqData)
+  })
+
+  socket.on('end', () => {
+    logger.log('Client disconnected')
+  })
+
+  socket.on('error', err => {
+    logger.error(err.message)
+  })
+})
+
+server.listen(port, host, () => {
+  logger.log(`Server running at ${host}:${port}`)
+})
